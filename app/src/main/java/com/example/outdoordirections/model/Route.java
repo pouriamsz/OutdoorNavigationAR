642ac6efffc53f2443da49a8eaaef6746496e618
p.mousazadeh <poriamsz55@gmail.com>
2023-07-21 17:22:15 +0330

    Add Route class

---
+++ app/src/main/java/com/example/outdoordirections/model/Route.java
@@ -0,0 +1,11 @@
+package com.example.outdoordirections.model;
+
+import java.util.ArrayList;
+
+public class Route {
+    ArrayList<Point> points;
+
+    public Route(ArrayList<Point> points){
+        this.points = points;
+    }
+}
